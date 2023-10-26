5047aaa838a4c99682e6796571c1b198bc505259
Diogo Souza <diogo.souza@msn.com>
2023-10-26 19:18:26 -0400

    corrige script para setar o utf8

--- script-database.sh
+++ script-database.sh
@@ -11,6 +11,6 @@ FOLDER_SCRIPTS="./sql/output"
 for script in "$FOLDER_SCRIPTS"/*; do
   if [ -f "$script" ]; then
     echo "Processing the SQL script: $(basename "$script")"
-    docker exec -i mysql mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -D $MYSQL_DATABASE <$script
+    docker exec -i mysql mysql -u$MYSQL_USER -p$MYSQL_PASSWORD --default-character-set=utf8mb4 -D $MYSQL_DATABASE <$script
   fi
 done
